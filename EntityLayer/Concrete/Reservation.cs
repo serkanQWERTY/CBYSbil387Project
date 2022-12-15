532cb3682341d028d74bd658b4d1d1636c7598c4
Serkan DEMİR <119077802+serkanQWERTY@users.noreply.github.com>
2022-12-15 23:02:55 +0300

    Add files via upload

---
+++ EntityLayer/Concrete/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using CoreLayer.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Reservation : IEntity
+    {
+        public int Id { get; set; }//PK
+        public DateTime ReservationTime { get; set; }
+        public int ClinicId { get; set; } // FK
+        public int DoctorId { get; set; } // FK
+        public int PatientId { get; set; } // FK
+        public int AssistantId{get; set; } // FK
+   
+    }
+}
